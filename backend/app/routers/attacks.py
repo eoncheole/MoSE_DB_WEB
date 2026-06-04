6f2cac087b58ac262126d02b3c72327936b41b6d
cheoleon (pstor) <26815428+eoncheole@users.noreply.github.com>
2026-06-04 15:09:23 +0900

    Update attacks.py

--- backend/app/routers/attacks.py
+++ backend/app/routers/attacks.py
@@ -4,7 +4,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from .. import crud, database, schemas
-from ..deps import get_current_active_user
+from ..deps import get_current_admin_user
 
 
 router = APIRouter(prefix="/attacks", tags=["Attacks"])
@@ -27,7 +27,7 @@ def get_attack(attack_id: int, db: Session = Depends(database.get_db)):
 def create_attack(
     attack: schemas.AttackTechniqueCreate,
     db: Session = Depends(database.get_db),
-    _: schemas.User = Depends(get_current_active_user),
+    _: schemas.User = Depends(get_current_admin_user),
 ):
     if crud.get_attack_by_name(db, attack.name):
         raise HTTPException(status_code=409, detail="Attack technique name already exists")
